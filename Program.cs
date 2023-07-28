7149e47ee355c64204de6b8f8e9c0371ec193c89
CalmMagma <100709631+CalmMagma@users.noreply.github.com>
2023-07-27 21:17:34 -0300

    v1.2 update

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 using LOR_DiceSystem;
 using LOR_XML;
-using System.Collections.Generic;
+
 
 namespace LoR_AutoLocalize
 {
@@ -11,134 +12,192 @@ namespace LoR_AutoLocalize
     {
         private static string h = "";
 
-        static string[] langList = new string[4] { "cn", "jp", "kr", "en" };
-
         static void Main()
         {
-            while (true)
-            {
-                Console.WriteLine("Insert the language of the exported folder (en, jp, kr, cn): ");
-                h = Console.ReadLine().ToLower();
-                if (langList.Contains(h))
-                    break;
-                else
-                    continue;
-            }
-            switch (h)
-            {
-                case "jp":
-                    Directory.CreateDirectory("jp");
-                    break;
 
-                case "cn":
-                    Directory.CreateDirectory("cn");
-                    break;
+            Console.WriteLine("Insert the language of the exported folder (en, jp, kr, cn, trcn, etc...): ");
+            h = Console.ReadLine().ToLower();
 
-                case "en":
-                    Directory.CreateDirectory("en");
-                    break;
-
-                case "kr":
-                    Directory.CreateDirectory("kr");
-                    break;
-            }
-            Console.WriteLine("Creating folders...");
-            Directory.CreateDirectory(h + "/BattleDialogues");
-            Directory.CreateDirectory(h + "/BattlesCards");
-            Directory.CreateDirectory(h + "/Books");
-            Directory.CreateDirectory(h + "/CharacterName");
-            Directory.CreateDirectory(h + "/Dropbooks");
-            Directory.CreateDirectory(h + "/PassiveDesc");
-            Directory.CreateDirectory(h + "/StageName");
-
-            Console.WriteLine("Creating localization XMLs...");
-            FileStream[] xmlList = new FileStream[7] {
-                File.Create(h + "/BattleDialogues/Dialogs.xml"),
-                File.Create(h + "/BattlesCards/CardNames.xml"),
-                File.Create(h + "/Books/BookDesc.xml"),
-                File.Create(h + "/CharacterName/Characters.xml"),
-                File.Create(h + "/Dropbooks/Dropbook.xml"),
-                File.Create(h + "/PassiveDesc/Passives.xml"),
-                File.Create(h + "/StageName/Stages.xml")
-            };
-
+            Directory.CreateDirectory(h);
             Console.WriteLine("Reading data XMLs...");
-            var dialogData = new XmlSerializer(typeof(BattleDialogRoot)).Deserialize(File.OpenRead("Combat_Dialog.xml")) as BattleDialogRoot;
-            var cardData = new XmlSerializer(typeof(DiceCardXmlRoot)).Deserialize(File.OpenRead("CardInfo.xml")) as DiceCardXmlRoot;
-            var bookDescData = new XmlSerializer(typeof(BookDescRoot)).Deserialize(File.OpenRead("BookStory.xml")) as BookDescRoot;
-            var enemyUnitData = new XmlSerializer(typeof(EnemyUnitClassRoot)).Deserialize(File.OpenRead("EnemyUnitInfo.xml")) as EnemyUnitClassRoot;
-            var dropbookData = new XmlSerializer(typeof(BookUseXmlRoot)).Deserialize(File.OpenRead("Dropbook.xml")) as BookUseXmlRoot;
-            var keypageData = new XmlSerializer(typeof(BookXmlRoot)).Deserialize(File.OpenRead("EquipPage_Librarian.xml")) as BookXmlRoot;
-            var stageData = new XmlSerializer(typeof(StageXmlRoot)).Deserialize(File.OpenRead("StageInfo.xml")) as StageXmlRoot;
-            var passiveData = new XmlSerializer(typeof(PassiveXmlRoot)).Deserialize(File.OpenRead("PassiveList.xml")) as PassiveXmlRoot;
 
-            Console.WriteLine("Converting XMLs...");
-            for (int i = 0; i < 7; i++)
+            try
             {
-                Console.WriteLine("> Localizing " + xmlList[i].Name);
-                switch (i)
+                if (File.Exists("Combat_Dialog.xml"))
                 {
-                    case 0:
-                        new XmlSerializer(typeof(BattleDialogRoot)).Serialize(xmlList[i], dialogData);
-                        break;
+                    var dialogData = new XmlSerializer(typeof(BattleDialogRoot)).Deserialize(File.OpenRead("Combat_Dialog.xml")) as BattleDialogRoot;
+                    Directory.CreateDirectory(h + "/BattleDialogues");
+                    FileStream file = File.Create(h + "/BattleDialogues/Dialogs.xml");
+                    Console.WriteLine("> Localizing " + file.Name);
 
-                    case 1:
-                        var localizeCards = new BattleCardDescRoot() { cardDescList = new List<BattleCardDesc>() };
-                        foreach (var card in cardData.cardXmlList)
-                        {
-                            localizeCards.cardDescList.Add(new BattleCardDesc() { cardID = card._id, cardName = card.workshopName});
-                        }
-                        new XmlSerializer(typeof(BattleCardDescRoot)).Serialize(xmlList[i], localizeCards);
-                        break;
+                    new XmlSerializer(typeof(BattleDialogRoot)).Serialize(file, dialogData);
 
-                    case 2:
+                    Console.WriteLine("- Localized " + file.Name);
+                    file.Close();
+                } else Console.WriteLine("! Combat_Dialog.xml was not found (it will not be localized)");
+
+
+                if (File.Exists("CardInfo.xml"))
+                {
+                    var cardData = new XmlSerializer(typeof(DiceCardXmlRoot)).Deserialize(File.OpenRead("CardInfo.xml")) as DiceCardXmlRoot;
+                    Directory.CreateDirectory(h + "/BattlesCards");
+                    var file = File.Create(h + "/BattlesCards/CardNames.xml");
+                    Console.WriteLine("> Localizing " + file.Name);
+
+                    var localizeCards = new BattleCardDescRoot() { cardDescList = new List<BattleCardDesc>() };
+                    foreach (var card in cardData.cardXmlList)
+                    {
+                        localizeCards.cardDescList.Add(new BattleCardDesc() { cardID = card._id, cardName = card.workshopName });
+                    }
+                    new XmlSerializer(typeof(BattleCardDescRoot)).Serialize(file, localizeCards);
+
+                    Console.WriteLine("- Localized " + file.Name);
+                    file.Close();
+                } else Console.WriteLine("! CardInfo.xml was not found (it will not be localized)");
+
+
+                if (File.Exists("BookStory.xml"))
+                {
+                    var bookDescData = new XmlSerializer(typeof(BookDescRoot)).Deserialize(File.OpenRead("BookStory.xml")) as BookDescRoot;
+                    Directory.CreateDirectory(h + "/Books");
+                    var file = File.Create(h + "/Books/BookDesc.xml");
+
+                    Console.WriteLine("> Localizing " + file.Name);
+
+                    if (File.Exists("EquipPage_Librarian.xml"))
+                    {
+                        var keypageData = new XmlSerializer(typeof(BookXmlRoot)).Deserialize(File.OpenRead("EquipPage_Librarian.xml")) as BookXmlRoot;
                         foreach (var guy in keypageData.bookXmlList)
                         {
                             var feller = bookDescData.bookDescList.Find(x => x.bookID == guy._id);
                             if (feller != null) feller.bookName = guy.InnerName;
                         }
-                        new XmlSerializer(typeof(BookDescRoot)).Serialize(xmlList[i], bookDescData);
+                    } else Console.WriteLine("! EquipPage_Librarian.xml not found- key page names may not be localized properly.");
+
+                    new XmlSerializer(typeof(BookDescRoot)).Serialize(file, bookDescData);
+
+                    Console.WriteLine("- Localized " + file.Name);
+                    file.Close();
+                } else Console.WriteLine("! EquipPage_Librarian.xml was not found (it will not be localized)");
+
+
+                if (File.Exists("EnemyUnitInfo.xml"))
+                {
+                    Directory.CreateDirectory(h + "/CharacterName");
+                    var enemyUnitData = new XmlSerializer(typeof(EnemyUnitClassRoot)).Deserialize(File.OpenRead("EnemyUnitInfo.xml")) as EnemyUnitClassRoot;
+                    var file = File.Create(h + "/CharacterName/Characters.xml");
+                    Console.WriteLine("> Localizing " + file.Name);
+
+                    var localizeChars = new CharactersNameRoot() { nameList = new List<CharacterName>() };
+                    foreach (var character in enemyUnitData.list)
+                    {
+                        localizeChars.nameList.Add(new CharacterName() { ID = character._id, name = character.name });
+                    }
+                    new XmlSerializer(typeof(CharactersNameRoot)).Serialize(file, localizeChars);
+
+                    Console.WriteLine("- Localized " + file.Name);
+                    file.Close();
+                } else Console.WriteLine("! EnemyUnitInfo.xml was not found (it will not be localized)");
+
+
+                if (File.Exists("Dropbook.xml"))
+                {
+                    Directory.CreateDirectory(h + "/Dropbooks");
+                    var dropbookData = new XmlSerializer(typeof(BookUseXmlRoot)).Deserialize(File.OpenRead("Dropbook.xml")) as BookUseXmlRoot;
+                    var file = File.Create(h + "/Dropbooks/Dropbook.xml");
+                    Console.WriteLine("> Localizing " + file.Name);
+
+                    var localizeBooks = new CharactersNameRoot() { nameList = new List<CharacterName>() };
+                    foreach (var book in dropbookData.bookXmlList)
+                    {
+                        localizeBooks.nameList.Add(new CharacterName() { ID = book._id, name = book.workshopName });
+                    }
+                    new XmlSerializer(typeof(CharactersNameRoot)).Serialize(file, localizeBooks);
+
+                    Console.WriteLine("- Localized " + file.Name);
+                    file.Close();
+                } else Console.WriteLine("! Dropbook.xml was not found (it will not be localized)");
+
+
+                if (File.Exists("StageInfo.xml"))
+                {
+                    Directory.CreateDirectory(h + "/StageName");
+                    var stageData = new XmlSerializer(typeof(StageXmlRoot)).Deserialize(File.OpenRead("StageInfo.xml")) as StageXmlRoot;
+                    var file = File.Create(h + "/StageName/Stages.xml");
+                    Console.WriteLine("> Localizing " + file.Name);
+
+                    var localizeStage = new CharactersNameRoot() { nameList = new List<CharacterName>() };
+                    foreach (var stage in stageData.list)
+                    {
+                        localizeStage.nameList.Add(new CharacterName() { ID = stage._id, name = stage.stageName });
+                    }
+                    new XmlSerializer(typeof(CharactersNameRoot)).Serialize(file, localizeStage);
+
+                    Console.WriteLine("- Localized " + file.Name);
+                    file.Close();
+                } else Console.WriteLine("! StageInfo.xml was not found (it will not be localized)");
+
+
+                if (File.Exists("PassiveList.xml"))
+                {
+                    Directory.CreateDirectory(h + "/PassiveDesc");
+                    var passiveData = new XmlSerializer(typeof(PassiveXmlRoot)).Deserialize(File.OpenRead("PassiveList.xml")) as PassiveXmlRoot;
+                    var file = File.Create(h + "/PassiveDesc/Passives.xml");
+                    Console.WriteLine("> Localizing " + file.Name);
+
+                    var localizePassives = new PassiveDescRoot() { descList = new List<PassiveDesc>() };
+                    foreach (var passive in passiveData.list)
+                    {
+                        localizePassives.descList.Add(new PassiveDesc() { _id = passive._id, desc = passive.desc, name = passive.name });
+                    }
+                    new XmlSerializer(typeof(PassiveDescRoot)).Serialize(file, localizePassives);
+
+                    Console.WriteLine("- Localized " + file.Name);
+                    file.Close();
+                } else Console.WriteLine("! PassiveList.xml was not found (it will not be localized)");
+
+            } catch (Exception e)
+            {
+                Console.WriteLine("There was an error while attempting to localize the XMLs: \n" + e.Message + "\n\nPress ENTER to close the program.");
+                Console.ReadLine();
+            }
+
+            /*
+            for (int i = 0; i < 7; i++)
+            {
+                
+                switch (i)
+                {
+                    case 0:
+                        
+                        break;
+
+                    case 1:
+                        
+                        break;
+
+                    case 2:
+                        
                         break;
 
                     case 3:
-                        var localizeChars = new CharactersNameRoot() { nameList = new List<CharacterName>() };
-                        foreach (var character in enemyUnitData.list)
-                        {
-                            localizeChars.nameList.Add(new CharacterName() { ID = character._id, name = character.name});
-                        }
-                        new XmlSerializer(typeof(CharactersNameRoot)).Serialize(xmlList[i], localizeChars);
-                        break;
+                        
 
                     case 4:
-                        var localizeBooks = new CharactersNameRoot() { nameList = new List<CharacterName>() };
-                        foreach (var book in dropbookData.bookXmlList)
-                        {
-                            localizeBooks.nameList.Add(new CharacterName() { ID = book._id, name = book.workshopName });
-                        }
-                        new XmlSerializer(typeof(CharactersNameRoot)).Serialize(xmlList[i], localizeBooks);
+
                         break;
 
                     case 5:
-                        var localizePassives = new PassiveDescRoot() { descList = new List<PassiveDesc>() };
-                        foreach (var passive in passiveData.list)
-                        {
-                            localizePassives.descList.Add(new PassiveDesc() { _id = passive._id, desc = passive.desc, name = passive.name });
-                        }
-                        new XmlSerializer(typeof(PassiveDescRoot)).Serialize(xmlList[i], localizePassives);
+                        
                         break;
 
                     case 6:
-                        var localizeStage = new CharactersNameRoot() { nameList = new List<CharacterName>() };
-                        foreach (var stage in stageData.list)
-                        {
-                            localizeStage.nameList.Add(new CharacterName() { ID = stage._id, name = stage.stageName });
-                        }
-                        new XmlSerializer(typeof(CharactersNameRoot)).Serialize(xmlList[i], localizeStage);
+                       
                         break;
                 }
-                Console.WriteLine("! Localized " + xmlList[i].Name);
-                xmlList[i].Close();
+                
             }
+            */
             Console.WriteLine("Finished auto-localization! Press ENTER to close this program...");
             Console.ReadLine();
         }
